b175e299628bd9fb65bca328b105bfe7afbe09f7
Devansh-Awasthi <devanshawasthi03@gmail.com>
2024-09-13 16:47:55 +0530

    data problrm fixed by intersecting data

--- src/Partials/Header.jsx
+++ src/Partials/Header.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios';
 function Header() {
   
+   const k= "95e6ba64";
+   var [v,setv]=useState([]);
 
     const clientId = 'e05d08fa373e1192315a37425be3a6d41c146336b973e4820c11fdb1b96b1f53'; 
+    
     const fetchPopularMovies = async () => {
+      var imdbIds;
       try {
         const response = await axios.get('https://api.trakt.tv/movies/popular', {
           headers: {
@@ -13,10 +17,28 @@ function Header() {
             'trakt-api-key':clientId
           }
         });
-        console.log('Popular Movies:', response.data);
-      } catch (error) {
+         imdbIds = response.data.map(movie => movie.ids.imdb);
+         } catch (error) {
         console.error('Error fetching popular movies:', error);
       }
+
+      console.log(imdbIds);
+      if (imdbIds) {
+        imdbIds.forEach(async(element) => {
+          try {
+            const y = await axios.get(
+              `http://www.omdbapi.com/?i=${element}&apikey=${k}`
+            );
+            if (y.data) {
+              console.log(y.data);
+             
+            }
+          } catch (error) {
+            console.log(error);
+          }
+        });
+   
+    }
     };
 
 
