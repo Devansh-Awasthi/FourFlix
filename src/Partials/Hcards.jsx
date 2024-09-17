57c30eb5dd630f2b3dbe850f21e66f8a3cc39777
Devansh-Awasthi <devanshawasthi03@gmail.com>
2024-09-17 16:51:14 +0530

    omdb connection left

--- src/Partials/Hcards.jsx
+++ src/Partials/Hcards.jsx
@@ -1,24 +1,38 @@
-import React from 'react'
-
+import React, { useEffect } from "react";
+import Call from "../Utils/Call";
 function Hcards() {
+  useEffect(() => {
+    const fetchTrending = async () => {
+      try {
+        const traktMovie = await Call.get("/movies/trending");
+        const traktShow = await Call.get("/shows/trending");
+        console.log(traktMovie.data);
+        console.log(traktShow.data);
+        traktMovie.data.map((d,i)=>{
+            
+        })
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    fetchTrending();
+  }, []);
 
-  
   return (
-    <div className='text-[#F1F1F1] h-[70vh] bg-[#181818] text-'>
-       <h1 className='text-[#F1F1F1] text-[4rem] font-bold'>Trending</h1> 
-       <div className='bg-red-400 flex gap-16 w-fit overflow-y-scroll items-center'>
-        <div className='bg-slate-300 h-96 w-40'>
-           </div>
-        <div className='bg-slate-300 h-40 w-40'> </div>
-        <div className='bg-slate-300 h-40 w-40'> </div>
-        <div className='bg-slate-300 h-40 w-40'> </div>
-        <div className='bg-slate-300 h-40 w-96'> </div>
-        <div className='bg-slate-300 h-40 w-40'> </div>
-        <div className='bg-slate-300 h-40 w-40'> </div>
-        <div className='bg-slate-300 h-40 w-40'> </div>
-       </div>
+    <div className="text-[#F1F1F1] h-[70vh] bg-[#181818] text-">
+      <h1 className="text-[#F1F1F1] text-[4rem] font-bold">Trending</h1>
+      <div className="bg-red-400 h-[50vh] flex gap-16 overflow-x-scroll w-full items-center">
+        <div className="bg-[#181818]  h-[80%] w-[30vw]"></div>
+        <div className=" h-40 w-40"></div>
+        <div className="h-40 w-40"></div>
+        <div className=" h-40 w-40"></div>
+        <div className="bg-slate-300 h-40 w-96"></div>
+        <div className="bg-slate-300 h-40 w-40"></div>
+        <div className="bg-slate-300 h-40 w-40"></div>
+        <div className="bg-slate-300 h-40 w-40"></div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hcards
+export default Hcards;
--- src/Utils/Call.jsx
+++ src/Utils/Call.jsx
@@ -1,5 +1,10 @@
-// import axios from "axios";
-// const Call=axios.create({
-//   baseUrl:''
-// })
-// export default Call;
+import axios from "axios";
+const Call=axios.create({
+  baseURL:'https://api.trakt.tv',
+    headers: {
+      "Content-Type": "application/json",
+      "trakt-api-version": "2",
+      "trakt-api-key": "e05d08fa373e1192315a37425be3a6d41c146336b973e4820c11fdb1b96b1f53"
+  }
+})
+export default Call;
