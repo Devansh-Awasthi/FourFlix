06fe13827fcbfe1db2cf622727fc18cd564e2715
Devansh-Awasthi <devanshawasthi03@gmail.com>
2024-09-18 20:05:04 +0530

    card started

--- src/Partials/Hcards.jsx
+++ src/Partials/Hcards.jsx
@@ -3,23 +3,32 @@ import Call from "../Utils/Call";
 import axios from "axios";
 function Hcards() {
   const k = "95e6ba64";
-    const [trending, setTrending] = useState([]);
+  const [trending, setTrending] = useState([]);
   useEffect(() => {
     const fetchTrending = async () => {
       try {
-        var traktMovie = await Call.get("/movies/trending");
-        var traktShow = await Call.get("/shows/trending");
-          
+        var traktMovie = await Call.get("/movies/trending", {
+          params: { limit: 20, page: 1 },
+        });
+        // var traktShow = await Call.get("/shows/trending");
+        // traktMovie.append(traktShow);
         console.log(traktMovie.data);
-        console.log(traktShow.data);
-        traktMovie.data.map( async(i)=>{
-        try
-        { const omdbMovie = await axios.get(`http://www.omdbapi.com/?i=${i.movie.ids.imdb}&apikey=${k}`)
-        } 
-        catch(e) {
-            console.error(e);
-        }
-       });
+        // console.log(traktShow.data);
+        setTrending(
+          await Promise.all(
+            traktMovie.data.map(async (i) => {
+              try {
+                const omdbMovie = await axios.get(
+                  `http://www.omdbapi.com/?i=${i.movie.ids.imdb}&apikey=${k}`
+                );
+                return omdbMovie;
+              } catch (e) {
+                console.error(e);
+                return null;
+              }
+            })
+          )
+        );
       } catch (e) {
         console.error(e);
       }
@@ -32,7 +41,11 @@ function Hcards() {
     <div className="text-[#F1F1F1] h-[70vh] bg-[#181818] text-">
       <h1 className="text-[#F1F1F1] text-[4rem] font-bold">Trending</h1>
       <div className="bg-red-400 h-[50vh] flex gap-16 overflow-x-scroll w-full items-center">
-        <div className="bg-[#181818]  h-[80%] w-[30vw]"></div>
+        {trending.map((item, index) => {
+          <div key={index} className="bg-[#181818] ml-5 h-[80%] w-[40vw]">
+            
+          </div>;
+        })}
         <div className=" h-40 w-40"></div>
         <div className="h-40 w-40"></div>
         <div className=" h-40 w-40"></div>
