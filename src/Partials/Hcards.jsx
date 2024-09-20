434d3acb59866218e0b418a1fe85e2162014456c
Devansh-Awasthi <devanshawasthi03@gmail.com>
2024-09-20 20:14:47 +0530

    filter functionality done

--- src/Partials/Hcards.jsx
+++ src/Partials/Hcards.jsx
@@ -2,52 +2,99 @@ import React, { useEffect, useState } from "react";
 import Call from "../Utils/Call";
 import axios from "axios";
 import { Link } from "react-router-dom";
+// import { data } from "autoprefixer";
 function Hcards() {
   const k = "95e6ba64";
   const [trending, setTrending] = useState([]);
-
+  var [button, setButton] = useState(false);
+  var [filter, setFilter] = useState("all");
   useEffect(() => {
     const fetchTrending = async () => {
+      var traktMovie;
+      var traktShow;
       try {
-        var traktMovie = await Call.get("/movies/trending", {
-          params: { limit: 20, page: 1 },
-        });
-        // var traktShow = await Call.get("/shows/trending");
-        // traktMovie.append(traktShow);
-        // console.log(traktMovie.data);
-        // console.log(traktShow.data);
+        if (filter === "all") {
+          traktMovie = await Call.get("/movies/trending");
+
+          //   , {
+          //   params: { limit: 20, page: 1 },
+          // });
+          traktShow = await Call.get("/shows/trending");
+        } else if (filter === "movies") {
+          traktMovie = await Call.get(
+            "/movies/trending",
+
+            {
+              params: { limit: 20, page: 1 },
+            }
+          );
+          traktShow = [];
+        } else if (filter === "shows") {
+          traktShow = await Call.get(
+            "/shows/trending",
+
+            {
+              params: { limit: 20, page: 1 },
+            }
+          );
+          traktMovie = [];
+        }
+        var final = [...traktMovie.data||[], ...traktShow.data||[]];
+        console.log(traktMovie.data);
+        console.log(traktShow.data);
 
         var b = await Promise.all(
-          traktMovie.data.map(async (i) => {
+          final.map(async (i) => {
             try {
-              const omdbMovie = await axios.get(
-                `http://www.omdbapi.com/?i=${i.movie.ids.imdb}&apikey=${k}`
-              );
-              return omdbMovie.data;
+              if (i.movie && i.movie.ids && i.movie.ids.imdb) {
+                const omdbMovie = await axios.get(
+                  `http://www.omdbapi.com/?i=${i.movie.ids.imdb}&apikey=${k}`
+                );
+                return omdbMovie.data;
+              } else {
+                const omdbShow = await axios.get(
+                  `http://www.omdbapi.com/?i=${i.show.ids.imdb}&apikey=${k}`
+                );
+                return omdbShow.data;
+              }
             } catch (e) {
               console.error(e);
               return null;
             }
           })
         );
-        console.log(b);
+
         setTrending(b.filter((movie) => movie !== null));
+        console.log(b);
       } catch (e) {
         console.error(e);
       }
     };
 
     fetchTrending();
-  }, []);
+  }, [filter]);
 
   return (
     <div className="text-[#F1F1F1] h-[70vh] w-full overflow-hidden bg-[#181818] ">
       <div className="flex mt-11 h-[4vh] w-[95%] items-center justify-between">
         <h1 className="text-[#F1F1F1] text-[4rem] font-bold">Trending</h1>
-        <button className="flex items-center bg-[#FF4500] pl-3 pr-3 rounded-2xl">
+        <button
+          onClick={() => setButton(!button)}
+          className="flex items-center bg-[#FF4500] pl-3 pr-3 rounded-2xl"
+        >
           <i className="ri-filter-2-fill text-[1.8rem]"></i>
-          <h1 className="text-[1.3rem] ml-2">filter</h1>
-          
+          {button &&
+            ["movies", "shows", "all"].map((a, i) => {
+              return (
+                <h1
+                  key={i}
+                  onClick={()=> setFilter(a)}
+                  className="text-[1.3rem] pl-3 pr-3 border-white border-x-[1px] ml-2"
+                >
+                  {a}
+                </h1>
+              );
+            })}
         </button>
       </div>
       <div className=" overflow-x-auto w-full">
