0bdb48deb88edd5c507497a4e631df9a84db8519
Devansh-Awasthi <devanshawasthi03@gmail.com>
2024-09-28 11:52:38 +0530

    popular repeat content resolved

--- src/Component/Popular.jsx
+++ src/Component/Popular.jsx
@@ -5,6 +5,7 @@ import Call from "../Utils/Call";
 import axios from "axios";
 import Card from "../Partials/Card";
 import InfiniteScroll from "react-infinite-scroll-component";
+// import Filter from "../Partials/Filter";
 
 function Popular() {
   document.title = "Movie App|Popular";
@@ -12,14 +13,17 @@ function Popular() {
   const OMDB_API_KEY = "95e6ba64";
   const [final, setFinal] = useState([]);
   const [page, setPage] = useState(1);
-  const [hasMore, setHasMore] = useState(true);
+  const [type, setType] = useState('movies');
+  const [hasMore, setHasMore] = useState(true);var a;
+
   const popular = async (page) => {
+    if(type==='movies') {
     try {
       const pop = await Call.get("/movies/popular", {
         params: { limit: 20, page: page },
       });
       console.log(pop.data);
-      var a = await Promise.all(
+       a = await Promise.all(
         pop.data.map(async (i) => {
           const response = await axios.get(
             `http://www.omdbapi.com/?i=${i.ids.imdb}&apikey=${OMDB_API_KEY}`
@@ -28,22 +32,46 @@ function Popular() {
           return response.data;
         })
       );
-      if (a.length === 0) {
-        setHasMore(false); // Stop infinite scroll
-        return;
-      }
-      // console.log(final);
-      setPage((prev) => prev + 1);
-
-      console.log(page);
-      setFinal((prev) => [...prev, ...a]);
     } catch (e) {
       console.error(e);
+    }}
+    else{
+      try {
+        const pop = await Call.get("/shows/popular", {
+          params: { limit: 20, page: page },
+        });
+        console.log(pop.data);
+         a = await Promise.all(
+          pop.data.map(async (i) => {
+            const response = await axios.get(
+              `http://www.omdbapi.com/?i=${i.ids.imdb}&apikey=${OMDB_API_KEY}`
+            );
+            // console.log(response);
+            return response.data;
+          })
+        );
+      }
+      catch (e) {
+        console.error(e);
+      
     }
+  }
+        if (a.length === 0) {
+          setHasMore(false); // Stop infinite scroll if no more results
+        } else {
+          setFinal((prev) => [...prev, ...a]);
+          setPage((prev) => prev + 1); // Increment page for next call
+        }
+       
   };
   useEffect(() => {
-    popular(page);
-  }, [page]);
+    
+    setFinal([]);
+    setPage(1)
+    popular(1);
+    setHasMore(true);
+    // setType('movies');
+  }, [type]);
   return (
     <div className="min-h-screen overflow-x-hidden bg-[#181818] w-screen text-[#F1F1F1] pl-8 pt-4">
       <div className="top w-screen flex items-center gap-16">
@@ -56,12 +84,30 @@ function Popular() {
         </div>
         <TopNav />
       </div>
+      <button
+      className="flex items-center rounded-2xl"
+    >
+      <i className={`ri-filter-2-fill rounded-xl flex bg-[#FF4500] pl-3 pr-3 text-[1.8rem]`}>
+        <h1 className="bg-[#FF4500] capitalize pl-3 pr-3">{type}</h1>
+      </i>
+      {
+        ['movies','shows'].map((a, i) => (
+          <h1
+            key={i}
+            onClick={() => setType(a)}
+            className="rounded-xl pt-1 pb-1 capitalize text-[1.6rem] bg-[#FF4500] pl-3 pr-3 ml-2"
+          >
+            {a}
+          </h1>
+        ))
+      }
+    </button>
       <InfiniteScroll
         dataLength={final.length}
         hasMore={hasMore}
         next={() => popular(page)}
       >
-        <div className="flex gap-12 max-h-fit w-screen overflow-auto flex-wrap">
+        <div className="flex gap-12 mt-7 max-h-fit w-screen overflow-auto flex-wrap">
           {final.map((item, i) => (
             <Card data={item} key={i} />
           ))}
