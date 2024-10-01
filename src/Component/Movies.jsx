053c55c7f2ae7ae87fd6e50e95f9c5210d140834
Devansh-Awasthi <devanshawasthi03@gmail.com>
2024-10-01 16:32:43 +0530

    Movies completed

--- src/Component/Movies.jsx
+++ src/Component/Movies.jsx
@@ -11,11 +11,12 @@ import Card from "../Partials/Card";
 
 function Movies() {
   const [final, setFinal] = useState([]);
-  // const [type,setType]=useState('Toprated');
+  const [type, setType] = useState("Top-100");
   //  const OMDB_API_KEY ="95e6ba64";
+  const [page, setPage] = useState(1);
   const n = useNavigate();
   const OMDB_API_KEY = "135eb90e";
-  var type = "popular";
+  // var type = "upcoming";
   const OmdbCall = async (id) => {
     try {
       const response = await axios.get(
@@ -27,8 +28,8 @@ function Movies() {
     }
   };
   const movies = async () => {
-    if (type === "Toprated") {
-      // const res= await Toprated.get();        // toprated
+    if (type === "Top-100") {
+      const res = await Toprated.get(); // toprated
       var f = await Promise.all(
         res.data.map(async (response) => {
           var b = await OmdbCall(response.imdbid);
@@ -36,10 +37,14 @@ function Movies() {
           return b;
         })
       );
-      setFinal(f);
+      console.log(f);
+      setFinal((p) => [...p, ...f]);
     } else if (type === "upcoming") {
-      var f;
-      const response1 = await Call.get("/movies/anticipated"); //upcoming
+      var f = [];
+      const response1 = await Call.get("/movies/anticipated", {
+        params: { limit: 20, page: page }, // upcoming
+        // params: { limit: 20 }, // upcoming
+      }); //upcoming
       console.log(response1);
       f = await Promise.all(
         response1.data.map(async (info) => {
@@ -47,11 +52,14 @@ function Movies() {
           return res;
         })
       );
-      // console.log(f);
-      setFinal(f);
+      console.log(f);
+      setFinal((p) => [...p, ...f]);
     } else if (type === "trending") {
-      var f;
-      const response2 = await Call.get("/movies/trending");
+      var f = [];
+      const response2 = await Call.get("/movies/trending", {
+        params: { limit: 20, page: page }, // upcoming
+        // params: { limit: 20 }, // upcoming
+      });
       console.log(response2);
       f = await Promise.all(
         response2.data.map(async (info) => {
@@ -59,17 +67,24 @@ function Movies() {
           return res;
         })
       );
+      // console.log(f);
       console.log(f);
+      setFinal((p) => [...p, ...f]);
     } else if (type === "popular") {
       var f = [];
-      const response = await ImdbMovies.get("getFanFavorites"); //popular /
+      const response = await Call.get("/movies/popular", {
+        params: { limit: 20, page: page }, // upcoming
+        // params: { limit: 20 }, // upcoming
+      });
+      console.log(response);
       f = await Promise.all(
-        response.data.data.list.map(async (info) => {
-          const res = await OmdbCall(info.id);
+        response.data.map(async (info) => {
+          const res = await OmdbCall(info.ids.imdb);
           return res;
         })
       );
       console.log(f);
+      setFinal((p) => [...p, ...f]);
       //    const response1 = await Call.get('/movies/popular');//popular
     }
 
@@ -79,7 +94,12 @@ function Movies() {
   };
   useEffect(() => {
     movies(); // Call the function on component mount
-  });
+  }, [page]);
+  useEffect(() => {
+    setFinal([]);
+    setPage(1);
+    movies();
+  }, [type]);
   return (
     <div className="min-h-screen overflow-x-hidden bg-[#181818] w-screen text-[#F1F1F1] pl-8 pt-4">
       <div className="top w-screen flex items-center gap-16">
@@ -92,17 +112,33 @@ function Movies() {
         </div>
         <TopNav />
       </div>
-      {/* <InfiniteScroll
+      <button className="flex items-center rounded-2xl">
+        <i
+          className={`ri-filter-2-fill rounded-xl flex bg-[#FF4500] pl-3 pr-3 text-[1.8rem]`}
+        >
+          <h1 className="bg-[#FF4500] capitalize pl-3 pr-3">{type}</h1>
+        </i>
+        {["Top-100", "trending", "popular", "upcoming"].map((a, i) => (
+          <h1
+            key={i}
+            onClick={() => setType(a)}
+            className="rounded-xl pt-1 pb-1 capitalize text-[1.6rem] bg-[#FF4500] pl-3 pr-3 ml-2"
+          >
+            {a}
+          </h1>
+        ))}
+      </button>
+      <InfiniteScroll
         dataLength={final.length}
-        hasMore={hasMore}
-        next={loadMoreData} 
+        hasMore={true}
+        next={() => setPage((page) => page + 1)}
       >
         <div className="flex gap-12 mt-7 max-h-fit w-screen overflow-auto flex-wrap">
           {final.map((item, i) => (
             <Card data={item} key={i} />
           ))}
         </div>
-      </InfiniteScroll> */}
+      </InfiniteScroll>
     </div>
   );
 }
